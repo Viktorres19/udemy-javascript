8c7589867624740deb092ff8572d45dc2aa38cf4
Viktor Lukyanchenkov <viktor.lukyanchenkov@cd.p2h.com>
2023-06-05 10:33:53 +0300

    Variables

--- script.js
+++ script.js
@@ -7,3 +7,16 @@
 /*console.log(3**2)*/ //9
 /*console.log(3**3)*/ //27
 /*console.log(2*(3+5)**2)*/ //128
+
+/*variables*/
+let marks
+
+marks = 50
+console.log(marks)
+marks = 100
+console.log(marks)
+
+let name
+
+name = "John"
+console.log(name)
