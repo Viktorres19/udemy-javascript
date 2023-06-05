67f79c122ea3cc7ebeaa9aff00692c3f79595e00
Viktor Lukyanchenkov <viktor.lukyanchenkov@cd.p2h.com>
2023-06-05 09:06:00 +0300

    Mathematical operations

---
+++ script.js
@@ -0,0 +1,9 @@
+/*Math operations*/
+/*console.log(3+3)*/
+/*console.log(4+3)*/
+/*console.log(4*3)*/
+/*console.log(12/3)*/
+/*console.log(12%3)*/ //0
+/*console.log(3**2)*/ //9
+/*console.log(3**3)*/ //27
+/*console.log(2*(3+5)**2)*/ //128
